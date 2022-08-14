5349c5581908c97e4f65b1849c81c53608c78d6b
Elias Gonzalez <48927897+red-peel@users.noreply.github.com>
2022-08-14 15:52:21 -0500

    Initialized app with Create React App

---
+++ new/src/App.js
@@ -0,0 +1,22 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      <div class="card" >
+  <img src="..." class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
